ae6ad30296a7acd076134043b5f499a7c119966b
I <henrikl@tuta.io>
2021-04-22 20:34:48 +0200

    general work

--- src/pages/playlist.jsx
+++ src/pages/playlist.jsx
@@ -1,11 +1,23 @@
 /*index.jsx*/
 import React from "react";
 import ExpandableMobileAppBar from "../components/expandable-mobile-app-bar";
+import RoundedTextAndImageButton from "../components/rounded-text-and-image-btn";
 //Functional Component 
+
+const playlistPlayButtonGroupStyle = {
+  display:"grid",
+  gridTemplateColumns:"auto auto"
+}
+
 const PlayList = () => {
   return (
     <div>
       <ExpandableMobileAppBar/>
+      <h2>Top 4</h2>
+      <div class="playlistPlayButtonGroup" style={playlistPlayButtonGroupStyle}>
+        <RoundedTextAndImageButton title="PLAY"/>
+        <RoundedTextAndImageButton title="SHUFFLE"/>
+      </div>
     </div>
   );
 };
