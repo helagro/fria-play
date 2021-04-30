74ead6bc66747ac6d777fa225b44e7e93f9684ff
I <henrikl@tuta.io>
2021-04-30 14:36:30 +0200

    more work on PlayControlsPc

--- src/components/PlayAndSwitchSong.jsx
+++ src/components/PlayAndSwitchSong.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import style from "./style/PlayAndSwitchSong.module.css"
 import playBtnIcon from "./img/logo23.svg"
 import nextSongIcon from "./img/next-song.svg"
+import prevSongIcon from "./img/prev-song.svg"
 
 function PlayAndSwitchSong() {
     return (
         <div className={style.container}>
-            <img src={nextSongIcon} alt="Prev song" className={style.prevSong}/>
-            <img src={playBtnIcon} alt="Play button" className={style.playBtn}/>
-            <img src={nextSongIcon} alt="Next song" className={style.nextSong}/>
+            <div className={style.btnContainer}><img src={prevSongIcon} alt="Prev song" className={style.prevSong}/></div>
+            <div className={style.btnContainer}><img src={playBtnIcon} alt="Play button" className={style.playBtn}/></div>
+            <div className={style.btnContainer}><img src={nextSongIcon} alt="Next song" className={style.nextSong}/></div>
         </div>
     )
 }
--- src/components/PlayControlsPc.jsx
+++ src/components/PlayControlsPc.jsx
@@ -3,16 +3,19 @@ import SongInfo from './SongInfo'
 import style from "./style/PlayControlsPc.module.css"
 import survival from "./img/survival.jpg"
 import PlayAndSwitchSong from "./PlayAndSwitchSong"
+import shuffleIcon from "./img/shuffle.svg"
+import moveDownIcon from "./img/moveDown.svg"
 
 function PlayControlsPc() {
     return (
         <div className={style.container}>
             <SongInfo image={survival} title="Survival" artist="eminem"/>
-            <div className="mainPlayerControls">
-                <PlayAndSwitchSong/>
-            </div>
-            <div className="advancedControls">
-
+            <PlayAndSwitchSong/>
+            <div className={style.advancedControls}>
+                <img src={moveDownIcon} alt="Move down"/>
+                <img src={shuffleIcon} alt="Shuffle"/>
+                <img src={shuffleIcon} alt="Shuffle"/>
+                <img src={shuffleIcon} alt="Shuffle"/>
             </div>
         </div>
     )
