96f168d72e99be97f6a756b9f948be5270415ecf
I <henrikl@tuta.io>
2021-05-02 12:39:32 +0200

    fixes playcontrolspc styling

--- src/components/PlayControlsPc.jsx
+++ src/components/PlayControlsPc.jsx
@@ -5,6 +5,8 @@ import survival from "./img/survival.jpg"
 import PlayAndSwitchSong from "./PlayAndSwitchSong"
 import shuffleIcon from "./img/shuffle.svg"
 import moveDownIcon from "./img/moveDown.svg"
+import replayIcon from "./img/ic-directions-round.svg"
+import volIcon from "./img/ic-media-volume.svg"
 
 function PlayControlsPc() {
     return (
@@ -14,8 +16,8 @@ function PlayControlsPc() {
             <div className={style.advancedControls}>
                 <img src={moveDownIcon} alt="Move down"/>
                 <img src={shuffleIcon} alt="Shuffle"/>
-                <img src={shuffleIcon} alt="Shuffle"/>
-                <img src={shuffleIcon} alt="Shuffle"/>
+                <img src={replayIcon} alt="replay"/>
+                <img src={volIcon} alt="volume"/>
             </div>
         </div>
     )
