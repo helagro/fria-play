9e0941e7a2c625c5affd736cbba50922da8ac994
I <henrikl@tuta.io>
2021-05-24 10:00:15 +0200

    rendering songs in list from state

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -2,15 +2,44 @@ import React from 'react'
 import Survival from "../components/img/survival.jpg"
 import SongInfo from "./SongInfo"
 import style from "./style/SongList.module.css"
+import googleDrive , {getDriveInstance} from "../logic/googleDriveApi"
 
+class SongList extends React.Component{
+    constructor(){
+        super()
 
-function SongList(props) {
-    return (
-        <div className={[style.container, props.className].join(" ")} style={props.style}>
-            <SongInfo image={Survival} title="Survival" artist="eminem"/>
-            <SongInfo image={Survival}/>
-        </div>
-    )
+        this.driveInstance = getDriveInstance(this.driveListener, this)
+
+        this.state = {
+            songs: [{
+                image: Survival,
+                title: "testtes",
+                artist: "init"
+            }]
+        }
+    }
+
+    driveInstance
+
+    render(){
+        let props = this.props
+
+        console.log("weird", this.state.songs)
+
+        return (
+            <div className={[style.container, props.className].join(" ")} style={props.style}>
+                {this.state.songs.map(({image, title, artist}) => 
+                    (<SongInfo image={image} title={title} artist={artist}/>)
+                )}
+            </div>
+        )
+    }
+
+    driveListener(containerCtx){
+        containerCtx.setState({
+            
+        })
+    }
 }
 
 export default SongList
